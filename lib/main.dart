cda57bfd98dea0e6d2e23dd2d8ab8b82eb472535
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-25 10:13:10 -0300

    floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi Bastião');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
