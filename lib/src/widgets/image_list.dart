3d05a6bd6c1be6047fe7496759478d1d0baf8a09
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-28 19:38:21 -0300

    add stateful wid

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
