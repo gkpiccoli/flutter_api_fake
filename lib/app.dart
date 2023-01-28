31c25cf62de0a00851036105f8a1b7fdb1d76e02
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-28 19:33:48 -0300

    add setState

--- lib/app.dart
+++ lib/app.dart
@@ -14,11 +14,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int count = 0;
 
+  List<ImageModel> images = [];
+
   Future<void> fetchImage() async {
     count++;
     var response =
         await get('https://jsonplaceholder.typicode.com/photos/$count' as Uri);
     var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   @override
