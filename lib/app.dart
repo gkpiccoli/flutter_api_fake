22fe1c227ccebde795eb0767cd91a4c47c021f6c
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-30 01:14:31 -0300

    try setstate

--- lib/app.dart
+++ lib/app.dart
@@ -17,12 +17,12 @@ class _MyAppState extends State<MyApp> {
   List<ImageModel> images = [];
 
   Future<void> fetchImage() async {
-    count++;
     var response = await get(
         'https://jsonplaceholder.typicode.com/photos/{$count}' as Uri);
     var imageModel = ImageModel.fromJson(json.decode(response.body));
     setState(() {
       images.add(imageModel);
+      count++;
     });
   }
 
